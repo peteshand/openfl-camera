7a49d543a21005cded980fc9f17fb1dfb08b6a6a
P.J.Shand <pete@peteshand.net>
2020-02-06 12:21:51 +1100

    add video getter

--- src/openfl/media/Camera.js.hx
+++ src/openfl/media/Camera.js.hx
@@ -3,7 +3,6 @@ package openfl.media;
 import haxe.ds.Either;
 import haxe.extern.EitherType;
 import haxe.Timer;
-
 import js.Promise;
 import js.Browser;
 import js.html.VideoElement;
@@ -11,7 +10,6 @@ import js.html.MediaStream;
 import js.html.MediaStreamTrack;
 import js.html.CanvasRenderingContext2D;
 import js.html.CanvasElement;
-
 import openfl.events.StatusEvent;
 import openfl.events.EventDispatcher;
 import openfl.geom.Rectangle;
@@ -20,266 +18,270 @@ import openfl.Vector;
 import openfl.display.BitmapData;
 import openfl.net.NetStream;
 import openfl.net.NetConnection;
-
 import lime.graphics.Image;
 import lime.graphics.ImageBuffer;
 
 @:access(openfl.display.BitmapData)
 @:access(openfl.net.NetStream)
-class Camera extends EventDispatcher
-{	
-    public var activityLevel(default, never) : Float;
-    public var bandwidth(default, never) : Int;
-    public var currentFPS(default, never) : Float;
-    public var fps(default, never) : Float;
-    public var height(default, never) : Int;
-    public var index(default, never) : Int;
-    public var isSupported(default, never) : Bool;
-    public var keyFrameInterval(default, never) : Int;
-    public var loopback(default, never) : Bool;
-    public var motionLevel(default, never) : Int;
-    public var motionTimeout(default, never) : Int;
-    public var muted(default, never) : Bool;
-    public var name(default, null) : String;
-    public static var names(default, null) : Array<String> = [];
-    public static var permissionStatus(default, never) : String;
-    public var position(default, never) : String;
-    public var quality(default, never) : Int;
-    public var width(default, never) : Int;
+class Camera extends EventDispatcher {
+	public var activityLevel(default, never):Float;
+	public var bandwidth(default, never):Int;
+	public var currentFPS(default, never):Float;
+	public var fps(default, never):Float;
+	public var height(default, null):Int = 320;
+	public var index(default, never):Int;
+	public var isSupported(default, never):Bool;
+	public var keyFrameInterval(default, never):Int;
+	public var loopback(default, never):Bool;
+	public var motionLevel(default, never):Int;
+	public var motionTimeout(default, never):Int;
+	public var muted(default, never):Bool;
+	public var name(default, null):String;
 
-    static var mediaDevices:MediaDevices;
-    static var devices = new Map<String, MediaDeviceInfo>();
-    var stream:MediaStream;
-    @:noCompletion var __video (default, null):VideoElement;
+	public static var names(default, null):Array<String> = [];
+	public static var permissionStatus(default, never):String;
 
-    var netStream:NetStream;
+	public var position(default, never):String;
+	public var quality(default, never):Int;
+	public var width(default, null):Int = 240;
 
-    var idealWidth:Null<Int>;
-    var idealHeight:Null<Int>;
-    var idealFps:Null<Float>;
-    var idealFavorArea:Bool = true;
+	static var mediaDevices:MediaDevices;
+	static var devices = new Map<String, MediaDeviceInfo>();
 
-    static function __init__()
-    {
-        mediaDevices = Reflect.getProperty(Browser.navigator, 'mediaDevices');
-        findAvailableDevices(true);
-    }
+	var stream:MediaStream;
+	@:noCompletion var __video(default, null):VideoElement;
 
-    private function new(name:String = null)
-    {
-        super();
-        this.name = name;
-        
-        mediaDevices.ondevicechange = ondevicechange;
+	var netStream:NetStream;
+	var video(get, null):VideoElement;
 
+	var idealWidth:Null<Int>;
+	var idealHeight:Null<Int>;
+	var idealFps:Null<Float>;
+	var idealFavorArea:Bool = true;
 
-        netStream = new NetStream(new NetConnection(), null);
-        netStream.__video.onloadedmetadata = function(e:Dynamic) {
-            netStream.__video.play();
-        };
-        
-        findAvailableDevices(findDevice);
-    }
+	static function __init__() {
+		mediaDevices = Reflect.getProperty(Browser.navigator, 'mediaDevices');
+		findAvailableDevices(true);
+	}
 
-    static function findAvailableDevices(callback:Void -> Void = null, updateNames:Bool=false)
-    {
-        var names:Array<String> = [];
-        devices = new Map<String, MediaDeviceInfo>();
+	private function new(name:String = null) {
+		super();
+		this.name = name;
 
-        mediaDevices.enumerateDevices().then(function(_devices:Array<MediaDeviceInfo>){
-            for (device in _devices){
-                if (device.kind == MediaDeviceKind.VIDEO_INPUT){
-                    names.push(device.label);
-                    devices.set(device.label, device);
-                }
-            }
-            if (updateNames) Camera.names = names;
-            if (callback != null) callback();
-        });
-    }
+		mediaDevices.ondevicechange = ondevicechange;
 
-    function ondevicechange(e:Dynamic)
-    {
-        clearActive();
-        Timer.delay(() -> {
-            findAvailableDevices(findDevice);
-        }, 100);
-    }
+		netStream = new NetStream(new NetConnection(), null);
+		netStream.__video.onloadedmetadata = function(e:Dynamic) {
+			netStream.__video.play();
+		};
 
-    function clearActive()
-    {   
-        if (this.stream != null){
-            var tracks:Array<MediaStreamTrack> = this.stream.getTracks();
-            for (track in tracks){
-                track.stop();
-                this.stream.removeTrack(track);
-            }
-            dispatchEvent(new StatusEvent(StatusEvent.STATUS, false, false, StatusEventCode.INACTIVE));
-        }
-        this.stream = null;
-        netStream.__video.srcObject = null;
-    }
+		findAvailableDevices(findDevice);
+	}
 
-    function findDevice()
-    {
-        var deviceInfo:MediaDeviceInfo = null;
-        if (name != null){
-            deviceInfo = devices.get(name);
-        }
-        
-        var constraints:MediaConstraints = { audio: false, video: true };
-        if (deviceInfo != null){
-            var trackConstraints:MediaTrackConstraints = {
-                deviceId: { exact: deviceInfo.deviceId },
-            };
-            if (idealWidth != null) trackConstraints.width = { ideal: idealWidth };
-            if (idealHeight != null) trackConstraints.height = { ideal: idealHeight };
-            if (idealFps != null) trackConstraints.frameRate = { ideal: idealFps };
-            constraints.video = trackConstraints;
-        } else if (name != null) {
-            trace("not device with name " + name + " found");
-            return;
-        }
-        
-        mediaDevices.getUserMedia(constraints).then(function(stream:MediaStream) {
-            this.stream = stream;
-            netStream.__video.srcObject = stream;
-            dispatchEvent(new StatusEvent(StatusEvent.STATUS, false, false, StatusEventCode.ACTIVE));
-        }).catchError(function(error:Dynamic) {
-            trace(error);
-        });
-    }
+	static function findAvailableDevices(callback:Void->Void = null, updateNames:Bool = false) {
+		var names:Array<String> = [];
+		if (devices == null) {
+			devices = new Map<String, MediaDeviceInfo>();
+		}
 
-    public function copyToByteArray(rect:Rectangle, destination:ByteArray):Void
-    {
-        var canvas = toCanvas(0, 0, Math.floor(rect.width), Math.floor(rect.height));
-        var bmd:BitmapData = BitmapData.fromCanvas(canvas, true);
-        var b = bmd.getPixels(rect);
-        destination.readBytes(b, 0, b.length);
-    }
+		mediaDevices.enumerateDevices().then(function(_devices:Array<MediaDeviceInfo>) {
+			for (device in _devices) {
+				if (device.kind == MediaDeviceKind.VIDEO_INPUT) {
+					names.push(device.label);
+					devices.set(device.label, device);
+					trace([device.label, device]);
+				}
+			}
+			if (updateNames)
+				Camera.names = names;
+			if (callback != null)
+				callback();
+		});
+	}
 
-    public function copyToVector(rect:Rectangle, destination:Vector<UInt>):Void
-    {
-        var canvas = toCanvas(0, 0, Math.floor(rect.width), Math.floor(rect.height));
-        var bmd:BitmapData = BitmapData.fromCanvas(canvas, true);
-        var v = bmd.getVector(rect);
-        for (i in 0...v.length){
-            destination.push(v[i]);
-        }
-    }
-    
-    public function drawToBitmapData(destination:BitmapData):Void
-    {
-        var canvas = toCanvas(0, 0, destination.width, destination.height);
-        var bmd:BitmapData = BitmapData.fromCanvas(canvas, destination.transparent);
-        destination.draw(bmd);
-    }
+	function ondevicechange(e:Dynamic) {
+		clearActive();
+		Timer.delay(() -> {
+			findAvailableDevices(findDevice);
+		}, 100);
+	}
 
-    function toCanvas(x:Int, y:Int, width:Int, height:Int)
-    {
-        var canvas:CanvasElement = Browser.document.createCanvasElement();
-        var ctx:CanvasRenderingContext2D = canvas.getContext('2d');
-        canvas.width = width;
-        canvas.height = height;
-        ctx.drawImage(netStream.__video, 0, 0);
-        return canvas;
-    }
+	function clearActive() {
+		if (this.stream != null) {
+			var tracks:Array<MediaStreamTrack> = this.stream.getTracks();
+			for (track in tracks) {
+				track.stop();
+				this.stream.removeTrack(track);
+			}
+			dispatchEvent(new StatusEvent(StatusEvent.STATUS, false, false, StatusEventCode.INACTIVE));
+		}
+		this.stream = null;
+		netStream.__video.srcObject = null;
+	}
 
-    
-    public static function getCamera(name:String = null):Camera
-    {
-        var cam = new Camera(name);
-        return cam;
-    }
-    
-    public function requestPermission():Void
-    {
-        throw "neess to be implemented";
-    }
-    
-    public function setKeyFrameInterval(keyFrameInterval:Int):Void
-    {
-        throw "neess to be implemented";
-    }
-    
-    public function setLoopback(compress:Bool = false):Void
-    {
-        throw "neess to be implemented";
-    }
-    
-    public function setMode(width:Int, height:Int, fps:Float, favorArea:Bool = true):Void
-    {
-        idealWidth = width; 
-        idealHeight = height;
-        idealFps = fps;
-        idealFavorArea = favorArea;
-    }
-    
-    public function setMotionLevel(motionLevel:Int, timeout:Int = 2000):Void
-    {
-        throw "neess to be implemented";
-    }
-    
-    public function setQuality(bandwidth:Int, quality:Int):Void
-    {
-        throw "neess to be implemented";
-    }
+	function findDevice() {
+		trace("findDevice");
+		var deviceInfo:MediaDeviceInfo = null;
+		if (name != null) {
+			deviceInfo = devices.get(name);
+		} else {
+			for (item in devices.iterator()) {
+				deviceInfo = item;
+				break;
+			}
+		}
+
+		var constraints:MediaConstraints = {audio: false, video: true};
+		if (deviceInfo != null) {
+			var trackConstraints:MediaTrackConstraints = {
+				deviceId: {exact: deviceInfo.deviceId},
+			};
+			if (idealWidth != null)
+				trackConstraints.width = {ideal: idealWidth};
+			if (idealHeight != null)
+				trackConstraints.height = {ideal: idealHeight};
+			if (idealFps != null)
+				trackConstraints.frameRate = {ideal: idealFps};
+			constraints.video = trackConstraints;
+		} else if (name != null) {
+			trace("not device with name " + name + " found");
+			return;
+		}
+		js.Browser.console.log("idealWidth = " + idealWidth);
+		js.Browser.console.log("idealHeight = " + idealHeight);
+		js.Browser.console.log("idealFps = " + idealFps);
+
+		js.Browser.console.log(deviceInfo);
+		js.Browser.console.log(constraints);
+		mediaDevices.getUserMedia(constraints).then(function(stream:MediaStream) {
+			trace("stream");
+
+			this.stream = stream;
+			netStream.__video.srcObject = stream;
+			dispatchEvent(new StatusEvent(StatusEvent.STATUS, false, false, StatusEventCode.ACTIVE));
+		}).catchError(function(error:Dynamic) {
+			trace(error);
+		});
+	}
+
+	public function copyToByteArray(rect:Rectangle, destination:ByteArray):Void {
+		var canvas = toCanvas(0, 0, Math.floor(rect.width), Math.floor(rect.height));
+		var bmd:BitmapData = BitmapData.fromCanvas(canvas, true);
+		var b = bmd.getPixels(rect);
+		destination.readBytes(b, 0, b.length);
+	}
+
+	public function copyToVector(rect:Rectangle, destination:Vector<UInt>):Void {
+		var canvas = toCanvas(0, 0, Math.floor(rect.width), Math.floor(rect.height));
+		var bmd:BitmapData = BitmapData.fromCanvas(canvas, true);
+		var v = bmd.getVector(rect);
+		for (i in 0...v.length) {
+			destination.push(v[i]);
+		}
+	}
+
+	public function drawToBitmapData(destination:BitmapData):Void {
+		var canvas = toCanvas(0, 0, destination.width, destination.height);
+		var bmd:BitmapData = BitmapData.fromCanvas(canvas, destination.transparent);
+		destination.draw(bmd);
+	}
+
+	function toCanvas(x:Int, y:Int, width:Int, height:Int) {
+		var canvas:CanvasElement = Browser.document.createCanvasElement();
+		var ctx:CanvasRenderingContext2D = canvas.getContext('2d');
+		canvas.width = width;
+		canvas.height = height;
+		ctx.drawImage(netStream.__video, 0, 0);
+		return canvas;
+	}
+
+	public static function getCamera(name:String = null):Camera {
+		var cam = new Camera(name);
+		return cam;
+	}
+
+	public function requestPermission():Void {
+		throw "neess to be implemented";
+	}
+
+	public function setKeyFrameInterval(keyFrameInterval:Int):Void {
+		throw "neess to be implemented";
+	}
+
+	public function setLoopback(compress:Bool = false):Void {
+		throw "neess to be implemented";
+	}
+
+	public function setMode(width:Int, height:Int, fps:Float, favorArea:Bool = true):Void {
+		trace("setMode");
+		this.width = idealWidth = width;
+		this.height = idealHeight = height;
+		idealFps = fps;
+		idealFavorArea = favorArea;
+	}
+
+	public function setMotionLevel(motionLevel:Int, timeout:Int = 2000):Void {
+		throw "neess to be implemented";
+	}
+
+	public function setQuality(bandwidth:Int, quality:Int):Void {
+		throw "neess to be implemented";
+	}
+
+	function get_video():VideoElement {
+		return netStream.__video;
+	}
 }
 
-typedef MediaDevices =
-{
-    dynamic function ondevicechange(event:Dynamic):Void;
-    function getUserMedia(constraints:Dynamic):Promise<MediaStream>;
-    function enumerateDevices():Promise<Array<MediaDeviceInfo>>;
-    function getSupportedConstraints():Array<Dynamic>;
+typedef MediaDevices = {
+	dynamic function ondevicechange(event:Dynamic):Void;
+	function getUserMedia(constraints:Dynamic):Promise<MediaStream>;
+	function enumerateDevices():Promise<Array<MediaDeviceInfo>>;
+	function getSupportedConstraints():Array<Dynamic>;
 }
 
-typedef MediaDeviceInfo =
-{
-    var deviceId(default, never):String;
-    var groupId(default, never):String;
-    var kind(default, never):MediaDeviceKind;
-    var label(default, never):String;
+typedef MediaDeviceInfo = {
+	var deviceId(default, never):String;
+	var groupId(default, never):String;
+	var kind(default, never):MediaDeviceKind;
+	var label(default, never):String;
 }
 
-typedef MediaConstraints =
-{
-    audio:EitherType<Bool, MediaTrackConstraints>,
-    video:EitherType<Bool, MediaTrackConstraints>
+typedef MediaConstraints = {
+	audio:EitherType<Bool, MediaTrackConstraints>,
+	video:EitherType<Bool, MediaTrackConstraints>
 }
 
-typedef MediaTrackConstraints =
-{
-    ?aspectRatio : Int, 
-	?brightness : Int, 
-	?channelCount : Int, 
-	?colorTemperature : Int, 
-	?contrast : Int, 
-	?deviceId : EitherType<Dynamic, String>, 
-	?echoCancellation : Bool, 
-	?exposureCompensation : Int, 
-	?exposureMode : String, 
-	?facingMode : String, 
-	?focusMode : String, 
-	?frameRate : EitherType<Dynamic, Int>, 
-	?groupId : EitherType<Dynamic, String>, 
-	?height : EitherType<Dynamic, Float>, 
-	?iso : Int, 
-	?latency : Float, 
-	?pointsOfInterest : Int, 
-	?sampleRate : Int, 
-	?sampleSize : Int, 
-	?saturation : Int, 
-	?sharpness : Int, 
-	?torch : Bool, 
-	?volume : Float, 
-	?whiteBalanceMode : String, 
-	?width : EitherType<Dynamic, Float>, 
-	?zoom : Int
+typedef MediaTrackConstraints = {
+	?aspectRatio:Int,
+	?brightness:Int,
+	?channelCount:Int,
+	?colorTemperature:Int,
+	?contrast:Int,
+	?deviceId:EitherType<Dynamic, String>,
+	?echoCancellation:Bool,
+	?exposureCompensation:Int,
+	?exposureMode:String,
+	?facingMode:String,
+	?focusMode:String,
+	?frameRate:EitherType<Dynamic, Int>,
+	?groupId:EitherType<Dynamic, String>,
+	?height:EitherType<Dynamic, Float>,
+	?iso:Int,
+	?latency:Float,
+	?pointsOfInterest:Int,
+	?sampleRate:Int,
+	?sampleSize:Int,
+	?saturation:Int,
+	?sharpness:Int,
+	?torch:Bool,
+	?volume:Float,
+	?whiteBalanceMode:String,
+	?width:EitherType<Dynamic, Float>,
+	?zoom:Int
 }
 
 @:enum abstract MediaDeviceKind(String) from String to String {
-	
 	public var AUDIO_INPUT = 'audioinput';
 	public var VIDEO_INPUT = 'videoinput';
 }
